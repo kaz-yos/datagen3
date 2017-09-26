6c0f90d1d27788e72ce8dac14dbad46aa700341e
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-09-26 15:16:01 -0400

    Add test

--- tests/testthat/test_datagen3.R
+++ tests/testthat/test_datagen3.R
@@ -16,11 +16,16 @@ test_that("rmultinom works", {
                        0,1,0,
                        0,0,1,
                        1/3,1/3,1/3),
+                     ncol = 3,
                      byrow = TRUE)
     A_mat <- pA_mat_to_A_mat(pA_mat)
 
     ## Expectations
     expect_equal(dim(pA_mat), dim(A_mat))
 
-    expect_true(FALSE)
+    A <- A_indicator_mat_to_multinom_A_vec(A_mat)
+
+    expect_equal(length(A), 4)
+    expect_equal(A[-4],c(0,1,2))
+
 })
