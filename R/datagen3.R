43d884e8310807ac2c708f9ad52f3a1fbed8e699
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-12-11 15:07:59 -0500

    Clean sub-list checker

--- R/datagen3.R
+++ R/datagen3.R
@@ -559,10 +559,10 @@ generate_scenario_data_frame <- function(lst_lst_possible_values) {
     ## All list elements must be lists.
     assertthat::assert_that(all(unlist(lapply(lst_lst_possible_values, class)) == "list"))
     ## All sub-list elements must be non-lists.
-    assertthat::assert_that(all(!unlist(lapply(lst_lst_possible_values,
-                                               function(lst) {
-                                                   lapply(lst, is.list)
-                                               }))))
+    assertthat::assert_that(all(unlist(lapply(lst_lst_possible_values,
+                                              function(lst) {
+                                                  all(!unlist(lapply(lst, is.list)))
+                                              }))))
 
     ## Expand scenarios to create all possible values.
     df <- do.call(expand.grid, lst_lst_possible_values) %>%
